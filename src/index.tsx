a26eb3e6ca55fe60c08cd4c6aef8bcaafc2eaae1
Andre Coelho <andre.ribeiro@iv2.com.br>
2020-01-22 16:00:16 -0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+
+const App: React.FC = () => (
+  <>
+    <StatusBar barStyle="dark-content" />
+    <SafeAreaView />
+  </>
+);
+
+export default App;
